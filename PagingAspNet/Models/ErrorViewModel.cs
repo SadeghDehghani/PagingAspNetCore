39f0415553405f971beac6137af5399794ad6c26
Sadegh <dehghani.prg@gmail.com>
2024-08-04 10:04:49 +0330

    Init

---
+++ PagingAspNet/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PagingAspNet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
